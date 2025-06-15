f25193bec39b6c2672954a6fa1e642668274e1b2
satyamgupta0704 <21bcs7408@cuchd.in>
2025-06-15 17:27:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_json():
+    with open('patients.json', 'r') as file:
+        return json.load(file)
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Management System"}
+
+@app.get("/greet/{name}")
+def greet(name: str):
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/view")
+def view():
+    data = load_json()
+    return data
